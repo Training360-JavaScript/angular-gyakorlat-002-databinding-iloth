d8101fc13ea12e99ea14ccc319e1c71777286a03
iloth <loth.istvan@hotmail.com>
2022-01-06 21:03:56 +0100

    Solution

--- src/app/pricing-page/pricing-page.component.ts
+++ src/app/pricing-page/pricing-page.component.ts
@@ -14,7 +14,14 @@ interface IPriceItem {
 })
 export class PricingPageComponent implements OnInit {
 
-  priceTable: IPriceItem[] = [];
+  priceTable: IPriceItem[] = [
+    {name: "Free", price: 0, btnText: "Ingyenes regisztráció", 
+      options: ["10 tanuló", "2 GB tárhely", "Email támogatás", "Dokumentáció"] },
+    {name: "Pro", price: 15, btnText: "Kezdő lépések", 
+      options: ["20 tanuló", "10 GB tárhely", "Kiemelt email támogatás", "Dokumentáció"] },
+    {name: "Enterprise", price: 29, btnText: "Kontakt", 
+      options: ["100 tanuló", "100 GB tárhely", "Telefonos és email támogatás", "Dokumentáció"] },
+  ];
 
   constructor() { }
 
